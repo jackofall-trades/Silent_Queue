5ae3ac5ac981de711b5a126bd7c619a2f6c4597e
RishiDevrana123 <rishidevrana2006@gmail.com>
2025-05-08 14:35:02 +0530

    somechanges

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': 'http://localhost:5000', // Redirect API calls to the backend
+    },
   },
 });
